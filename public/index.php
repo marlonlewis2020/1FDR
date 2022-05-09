ddbcf90ca9065d5f6f2ba6480b78a7cdc977f8bb
Marlon Lewis <marlonlewis483@gmail.com>
2022-05-09 00:13:40 -0500

    updated sites

---
+++ public/index.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+
+  <?php include "header.html"; ?>
+
+
+  <body>
+
+    <!-- ===============================================-->
+    <!--    Main Content-->
+    <!-- ===============================================-->
+    <main class="main" id="top">
+    <?php include "navbar.html"; ?>
+
+    <!-- ===============================================-->
+    <!--       START CONTENT     -->
+    <!-- ===============================================-->
+
+      <div id="1fdr content">
+        <?php include "banner.html"; ?>
+
+        <?php include "menu.html"; ?>
+
+        <?php include "home.html"; ?>
+      </div>
+
+
+    <!-- ===============================================-->
+    <!--       END CONTENT     -->
+    <!-- ===============================================-->
+
+   <?php include "footer.html"; ?>
+
+   </main>
+    <!-- ===============================================-->
+    <!--    End of Main Content-->
+    <!-- ===============================================-->
+
+    <?php include "javascripts.html"; ?>
+
+  </body>
+
+</html>
